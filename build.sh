e5bfe80c5a571122adcbbfd07405493838f47fd2
hub <code@hub.lol>
2022-08-28 23:23:36 +0200

    todo

--- build.sh
+++ build.sh
@@ -13,6 +13,7 @@ done
 # build mrpack
 packwiz refresh
 packwiz modrinth export
+# TODO: include the overrides normally when packwiz introduces environment side support for internal files
 zip --update --recurse-paths \
     "$(find -- * -type f -iname '*.mrpack' | head -n1)" \
     "overrides" "client-overrides" "server-overrides"
